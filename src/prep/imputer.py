4126779f3f3445b3d7eb9e511d4e7d1ae00ae0b8
Rohmatul Iman <rohmatulimn@gmail.com>
2026-04-18 18:24:39 +0700

    Debug 1: Imputer

--- src/prep/imputer.py
+++ src/prep/imputer.py
@@ -11,16 +11,22 @@ class DataImputer:
         self.categorical_cols = df.select_dtypes(include=["object", "category"]).columns
 
         self.means = df[self.numeric_cols].mean()
-        self.modes = df[self.categorical_cols].mode().iloc[0]
+
+        # handle kalau tidak ada kolom kategorik
+        if len(self.categorical_cols) > 0:
+            self.modes = df[self.categorical_cols].mode().iloc[0]
+        else:
+            self.modes = pd.Series(dtype=object)
 
     def transform(self, df: pd.DataFrame):
         df = df.copy()
 
-        # isi numerik dengan mean
+        # numerik
         df[self.numeric_cols] = df[self.numeric_cols].fillna(self.means)
 
-        # isi kategorik dengan modus
-        df[self.categorical_cols] = df[self.categorical_cols].fillna(self.modes)
+        # kategorik (kalau ada)
+        if len(self.categorical_cols) > 0:
+            df[self.categorical_cols] = df[self.categorical_cols].fillna(self.modes)
 
         return df
 
